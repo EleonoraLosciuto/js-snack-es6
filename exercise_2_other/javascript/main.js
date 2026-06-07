0575a4a4f1ed24654ce80fba042c835b5f62662f
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-06-07 21:19:43 +0200

    exercise 2 - create array and style.css

--- exercise_2_other/javascript/main.js
+++ exercise_2_other/javascript/main.js
@@ -1,1 +1,20 @@
-const makeBike 
+// Factory Function per creare squadra
+const makeTeam = (name, points, foulsWon) => ({
+name, points, foulsWon
+});
+
+// creo array utilizzando la mia factory function e un numero random() per punti e falli considerando che i punti possono essere tra 0 e 59 e falli tra 0 e 99
+const teamList = [
+    makeTeam("Juventus", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Milan", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Inter", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Roma", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Lazio", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Torino", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Udinese", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Sassuolo", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Udinese", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    makeTeam("Napoli", Math.floor(Math.random() * 60), Math.floor(Math.random() * 100)),
+    
+];
+    console.log(teamList);
